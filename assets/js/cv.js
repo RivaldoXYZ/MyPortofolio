34e05223bde34ef58a25dc53e181864130730597
RivaldoXYZ <ce322033@students.del.ac.id>
2024-12-06 21:14:36 +0700

    Change All Template

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -1,32 +1,31 @@
+const pdfUrl = '/assets/cv/Rivaldo Butarbutar-resume.pdf'; // Mendeklarasikan variabel pdfUrl
 
-        const pdfUrl = '/assets/cv/Rivaldo Butarbutar-resume.pdf';
-        // Fungsi untuk menampilkan PDF
-        function renderPDF(url) {
-            // Memuat dokumen PDF
-            pdfjsLib.getDocument(url).promise.then(function (pdfDoc_) {
-                const pdfDoc = pdfDoc_;
-                const pageNumber = 1; // Menampilkan halaman pertama
+// Fungsi untuk menampilkan PDF
+function renderPDF(url) {
+    pdfjsLib.getDocument(url).promise.then(function (pdfDoc_) {
+        const pdfDoc = pdfDoc_;
+        const pageNumber = 1; // Menampilkan halaman pertama
 
-                pdfDoc.getPage(pageNumber).then(function (page) {
-                    const scale = 1.5;
-                    const viewport = page.getViewport({ scale: scale });
+        pdfDoc.getPage(pageNumber).then(function (page) {
+            const scale = 1.5;
+            const viewport = page.getViewport({ scale: scale });
 
-                    // Ambil canvas DOM untuk menampilkan PDF
-                    const canvas = document.getElementById('pdfCanvas');
-                    const context = canvas.getContext('2d');
-                    canvas.height = viewport.height;
-                    canvas.width = viewport.width;
+            // Ambil canvas DOM untuk menampilkan PDF
+            const canvas = document.getElementById('pdfCanvas');
+            const context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
 
-                    // Render PDF ke canvas
-                    page.render({
-                        canvasContext: context,
-                        viewport: viewport
-                    });
-                });
+            // Render PDF ke canvas
+            page.render({
+                canvasContext: context,
+                viewport: viewport
             });
-        }
+        });
+    });
+}
 
-        // Panggil fungsi renderPDF saat halaman dimuat
-        window.onload = function () {
-            renderPDF(pdfUrl);
-        };
+// Panggil fungsi renderPDF saat halaman dimuat
+window.onload = function () {
+    renderPDF(pdfUrl);  // Menjamin pdfUrl sudah didefinisikan sebelum dipanggil
+};
